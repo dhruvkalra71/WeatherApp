0f70ebb24c4da3a92df82f5b3c41f6412e971661
Dhruv Kalra <166701601+dhruvkalra71@users.noreply.github.com>
2025-06-12 16:10:22 +0530

    Create WeatherApp.py

---
+++ WeatherApp.py
@@ -0,0 +1,25 @@
+import requests
+
+print("Welcome to Weather App!")
+city_name = input("Enter city name: ")
+API_key = '7295ed33a7cfb8f84f8684f61a030f8b'
+
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric'
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    #print(data)
+    print("City Name = ", data['name'])
+
+    print(f'Longitude = {data['coord']['lon']} and Latitude = {data['coord']['lat']}')
+    print(f'Weather is {data['weather'][0]['main']}')
+    print(f'Temperature = {data['main']['temp']}')
+    print(f'Feels like {data['main']['feels_like']}')
+    print(f'Temperature Minimum = {data['main']['temp_min']}')
+    print(f'Temperature Maximum = {data['main']['temp_max']}')
+    print(f'Pressure = {data['main']['pressure']}')
+    print(f'Humidity = {data['main']['humidity']}')
+    print(f'Sea_level = {data['main']['sea_level']}')
+    print(f'Ground Level = {data['main']['grnd_level']}')
+    print(f'Visibility = {data['visibility']}')
